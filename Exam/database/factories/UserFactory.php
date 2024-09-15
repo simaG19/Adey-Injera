86650d50cc5e93ee58208f48dc12f57829e6af10
simon <simonwubshet254@gmail.com>
2024-09-15 19:27:28 +0300

    User Factory

---
+++ Exam/database/factories/UserFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(), // Ensure emails are unique if needed
+            'email_verified_at' => now(),
+            'role' => fake()->numberBetween(1, 2), // Remove uniqueness constraint if not needed
+            'password' => Hash::make('password'), // Use a static password if desired
+            'remember_token' => Str::random(10),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
