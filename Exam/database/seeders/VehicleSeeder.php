14b01fe37ec87a16d5accf8476346d111575323c
simon <simonwubshet254@gmail.com>
2024-09-15 19:26:46 +0300

    Database seeding

---
+++ Exam/database/seeders/VehicleSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Vehicle;
+
+class VehicleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Create 50 vehicles using the factory
+        Vehicle::factory()->count(50)->create();
+    }
+}
+
