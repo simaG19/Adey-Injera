f36c6aa5e27b1e26d48436e65ddb6ba1ee92b2ac
simon <88380975+simaG19@users.noreply.github.com>
2024-10-21 10:16:19 +0300

    to show the enjera package

--- Exam/resources/views/vehicle/show.blade.php
+++ Exam/resources/views/vehicle/show.blade.php
@@ -1,18 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
+
     <div class="container">
-        <h1>Vehicle Details</h1>
+        <h1> Adey Enjera </h1>
 
         <div class="card">
             <div class="card-header">
-                Vehicle ID: {{ $vehicle->id }}
+                <h3>Enjera Package Details</h3>
             </div>
             <div class="card-body">
-                <h5 class="card-title">{{ $vehicle->make }} {{ $vehicle->model }}</h5>
-                <p class="card-text"><strong>Year:</strong> {{ $vehicle->year }}</p>
-                <p class="card-text"><strong>License Plate:</strong> {{ $vehicle->license_plate }}</p>
-                <a href="{{ route('driver') }}" class="btn btn-primary">Back to List</a>
+                <p class="card-text"><strong>Type:</strong> {{ $vehicle->make }}</p>
+                <p class="card-text"><strong>Amount:</strong> {{ $vehicle->model }}</p>
+                <p class="card-text"><strong>Price:</strong> {{ $vehicle->year }}</p>
+
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#contactModal">
+                    Order
+                </button>
+            </div>
+        </div>
+
+        <!-- Contact Modal -->
+        <div class="modal fade" id="contactModal" tabindex="-1" role="dialog" aria-labelledby="contactModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="contactModalLabel">Contact Us</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="list-group">
+                            <a href="tel:+251900000000" class="list-group-item list-group-item-action">
+                                <i class="fas fa-phone-alt"></i> +251 90 000 0000
+                            </a>
+                            <a href="mailto:info@adeyenjera.com" class="list-group-item list-group-item-action">
+                                <i class="fas fa-envelope"></i> info@adeyenjera.com
+                            </a>
+                            <a href="https://www.example.com" target="_blank" class="list-group-item list-group-item-action">
+                                <i class="fas fa-globe"></i> www.adeyenjera.com
+                            </a>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
